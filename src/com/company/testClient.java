99ffd29d3b671483fc6cc79281403e09739c3dab
MarshallDu0204 <2440075115@qq.com>
2019-10-17 13:15:23 -0400

    2.2
    Add udpTester

--- src/com/company/testClient.java
+++ src/com/company/testClient.java
@@ -5,9 +5,16 @@ import java.io.IOException;
 public class testClient {
 
     public void init() throws IOException {
+        /*
         tcpSenderTest sender = new tcpSenderTest();
         tcpReceiverTest receiver = new tcpReceiverTest(52060);
         sender.send("eecslab-11.case.edu",52060,"Hello\n");//MUST ADD \n AT THE END OF THE MESSAGE
+        receiver.receive();*/
+
+        udpSenderTest sender = new udpSenderTest();
+        udpReceiverTest receiver = new udpReceiverTest(52060);
+
+        sender.send("Hello\n","eecslab-13.case.edu",52060);
         receiver.receive();
     }
 
--- src/com/company/udpReceiverTest.java
+++ src/com/company/udpReceiverTest.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class udpReceiverTest {
+    private static int portNum;
+
+    public udpReceiverTest(int port){
+        portNum = port;
+    }
+
+    public void receive() throws IOException {
+        DatagramSocket recvSocket = new DatagramSocket(portNum);
+
+        String message = "";
+
+        byte[] recvData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        while (true) {
+            DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
+
+            recvSocket.receive(recvPacket);
+
+            message = new String(recvPacket.getData());
+
+            InetAddress addr = recvPacket.getAddress();
+            int port = recvPacket.getPort();
+
+            System.out.println(message);
+
+            message = "response";
+
+            //String[] ping = message.split(":");
+
+            // if connection is full then only flood the query
+            // else return the pong message and flood the query
+
+            sendData = message.getBytes();
+
+            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, addr, port);
+
+            recvSocket.send(sendPkt);
+        }
+
+    }
+
 }
